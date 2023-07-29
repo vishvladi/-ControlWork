a9590719c6f6b52975d202d85e3e5036f0602e15
vishvladi <vlad-vishnevski@mail.ru>
2023-07-29 22:22:09 +0300

    Getting 3-chars strings array from the initial one

---
+++ Task/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив 
+// из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно
+//  ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+string[] array1 = new string[5] {"program", ":)", "Tank", "wow", "1689"};
+string[] array2 = new string[array1.Length];
+void ChosenStringsArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t ");
+    }
+    Console.WriteLine();
+}
+ChosenStringsArray(array1, array2);
+PrintArray(array2);
+     
